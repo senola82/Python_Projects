aac375231adbbe035a17e5384d8d6bb5fee83ca9
senola82 <senol.a82@hotmail.com>
2023-06-01 00:31:41 +0300

    Update KİRİŞ METODU SECANT.py

--- KİRİŞ METODU SECANT.py
+++ KİRİŞ METODU SECANT.py
@@ -6,8 +6,6 @@ Created on Sun Apr  9 01:51:59 2023
 """
 
 
-print("f(xz)*f(x2)<0 ifadesini kontrol et")
-print("ÖNCE KÖKÜN VARLIĞINI KONTROL ET")
 
 import math
 e = math.e
@@ -22,9 +20,32 @@ N = 100
 # function we are using
 def f(x):
     #return (e**x/x)-(sin(x)/x**2)+(2*x)-10#POLİNOM
-    return cos(x)+sin(1+x**2)-1
+   # return cos(x)+sin(1+x**2)-1
+    return (e**-x)-x
+print("""
+      
+ÖNCE TEMEL FORMÜLÜ KULLANABİLMEK İÇİN FONKSİYONUN X E GÖRE TÜREVİ ALINIR 
 
-print("n", "        Xi-1", "           Xi", "           Xi+1", "         f(i-1)", "        f(i)")
+     -------------------FORMÜL-------------------
+
+                           f(Xi)*(Xi-1 - Xi)
+             Xi+1 = Xi - --------------------
+                           f(Xi-1) - f(Xi) 
+     --------------------------------------------     
+     
+     xi-1 = X-1 = 0.0    ve     xi = X0 = 1.0 BAŞLANGIÇ DEĞERİ İSE
+     
+     fonksiyonumuz f(x) = e**-x -x olsun.  
+     
+     buradan  
+     f(xi)= f(x0)= e**-1-1 = 1/e - 1     f(xi-1)= f(x-1)=-e**-0 = 1
+     
+                       (-0,63212055)*(0 - 1)
+     Xi+1 = x1 = 1 -  ---------------------- = 0,61270
+                        1 - (-0,63212055)
+     """)
+
+print("\nn", "      Xi-1", "           Xi", "          Xi+1", "         f(xi-1)", "          f(xi)")
 # Actual Secant Method
 def secant(p0, p1, tol, N):
     for i in range(0, N):
@@ -40,7 +61,7 @@ def secant(p0, p1, tol, N):
  
 
 
-print("iterasyon:",secant(0,3,pow(10,-6),100))#x-1 , x0,
+print("\niterasyon:",secant(0,1,pow(10,-6),100))#x-1 , x0,
 
 
 
